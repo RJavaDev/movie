dc0a42915b03a214e68cbc7d22411d0f0ea65732
Sayidalokhon <sayidalokhonkham@gmail.com>
2023-01-26 18:52:35 +0500

    data base done

--- src/main/java/org/example/entity/MovieEntity.java
+++ src/main/java/org/example/entity/MovieEntity.java
@@ -1,12 +1,7 @@
 package org.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.List;
 
 
 @AllArgsConstructor
